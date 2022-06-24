7ecccef70f4d504b807704006b4c49e2091c993c
k2h2j3 <74819625+k2h2j3@users.noreply.github.com>
2022-06-24 11:18:14 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -4,13 +4,27 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 import './App.css';
 
-
+ 
 class App extends Component {
+  constructor(props){ //props를 초기화//
+    super(props); //constructor 내부에서 props를 작성//
+    this.state = { //state의 값을 초기화// 
+      subject:{title:'WEB', sub:'World wide web!'}, //subject의 값을 state화//
+      contents:[ //여러개의 state를 다룰때는 배열사용//
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title= {this.state.subject.title} //state 값을 준다//
+          sub={this.state.subject.sub}>
+        </Subject>
+        <TOC data={this.state.contents}></TOC> 
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );   
--- components/TOC.js
+++ components/TOC.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 
 class TOC extends Component {
     render(){
+        var lists = [];
+        var data = this.props.data;
+        var i = 0;
+        while(i < data.length){
+            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>); //자동으로 elements 생성, 각 element는 key값을 가져야하므로 li 뒤에 key를 붙임//
+            i = i + 1;
+        }
       return (
           <nav>
               <ul>
-                  <li><a href ="1.html">HTML</a></li>
-                  <li><a href ="2.html">CSS</a></li>
-                  <li><a href ="3.html">JavaScript</a></li>
+                  {lists}
               </ul>
           </nav>
       );
     }
   }
 
-  export default TOC;
+export default TOC;
